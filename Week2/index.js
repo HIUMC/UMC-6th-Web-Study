d8d8fdfd366848499d56c43f2cf60ed67693be7f
leve68 <hanstom000608@gmail.com>
2024-04-04 19:34:44 +0900

    Week 2

---
+++ Week2/index.js
@@ -0,0 +1,11 @@
+let number = document.getElementById("number");
+let inc = document.getElementById("increase");
+let dec = document.getElementById("decrease");
+
+function increase() {
+  number.innerHTML++;
+}
+
+function decrease() {
+  number.innerText--;
+}
