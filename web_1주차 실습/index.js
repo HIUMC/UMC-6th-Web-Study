7467345b052cce634053fb25c4c3f34ee648a264
woni <jsw321jsw321@naver.com>
2024-04-04 20:03:38 +0900

    1주차 실습

---
+++ web_1주차 실습/index.js
@@ -0,0 +1,17 @@
+const number = document.getElementById("number");
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("decrease");
+
+console.log(number);
+console.log(increase);
+console.log(decrease);
+
+increase.onclick = () => {
+  const current = parseInt(number.innerText, 10);
+  number.innerText = current + 1;
+};
+
+decrease.onclick = () => {
+  const current = parseInt(number.innerText, 10);
+  number.innerText = current - 1;
+};
