710e4ca2b97bb8eca3bfc331d75851e443889eec
challonsy <musain2019@naver.com>
2024-04-14 14:53:31 +0900

    모달 수정, css 수정 전

---
+++ 2주차/Modal/modal.js
@@ -0,0 +1,10 @@
+const modal = document.getElementById("modal");
+const screen = document.getElementById("screen");
+
+function openBtn() {
+  modal.style.display = "";
+}
+
+function closeBtn() {
+  modal.style.display = "none";
+}
