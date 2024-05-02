64d4474d95b5fd6c9eb4cc2faed16cedec4fdb03
박형진 <gud5223@naver.com>
2024-05-02 16:52:21 +0900

    hw2

---
+++ hw2/index.js
@@ -0,0 +1,8 @@
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+open.onclick = () => {
+  modal.style.display = "flex";
+};
+close.onclick = () => {
+  modal.style.display = "none";
+};
