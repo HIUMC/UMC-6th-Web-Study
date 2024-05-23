916bd57230a7b6c962c52c589f4c127e1099c320
YangYoonseo <yys0847@naver.com>
2024-05-23 11:40:03 +0900

    감정일기장

---
+++ ONEBITE-REACT/section12/src/pages/New.jsx
@@ -0,0 +1,30 @@
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import Header from "../components/Header";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
+
+const New = () => {
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true });
+  };
+
+  return (
+    <div>
+      <div>
+        <Header
+          title={"새 일기 쓰기"}
+          leftChild={<Button text={"< 뒤로가기"} onClick={() => nav(-1)} />}
+        />
+        <Editor onSubmit={onSubmit} />
+      </div>
+    </div>
+  );
+};
+
+export default New;
