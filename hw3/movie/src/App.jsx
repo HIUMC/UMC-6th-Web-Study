978f59aa8a0c322aa4ff133e4aa9ad0f81082268
박형진 <gud5223@naver.com>
2024-05-23 17:23:52 +0900

    영화

--- hw3/movie/src/App.jsx
+++ hw3/movie/src/App.jsx
@@ -1,6 +1,36 @@
-import Content from "./components/Content";
+import "./App.css";
+import Movie from "./components/Movie";
+import styled from "styled-components";
+import { movies } from "./components/MovieDummy";
+
+export const Body = styled.div`
+  background-color: #242756;
+`;
+
+export const AppContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
 function App() {
-  return <Content />;
+  return (
+    <Body>
+      <AppContainer>
+        {movies.results.map((item) => {
+          return (
+            <Movie
+              key={item.id}
+              title={item.title}
+              poster_path={item.poster_path}
+              vote_average={item.vote_average}
+              overview={item.overview}
+            />
+          );
+        })}
+      </AppContainer>
+    </Body>
+  );
 }
 
 export default App;
